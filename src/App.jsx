1a96c088a287d8ed11eb9062ce4f97ebc7010023
David <davigonzalezdev@gmail.com>
2025-09-09 15:16:16 -0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
 import { useEffect, useState } from "react";
 import "./App.css"
 
-function Card({updateBoard,index}) {
+function Card({updateBoard,index,pokemon}) {
 
    const handleClick = () => {
     updateBoard(index)
   }
-  const [pokemon, setPokemon] = useState({});
-
-  useEffect(() => {
-    let ignore = false;
-    const fetchResults = async () => {
-      try {
-        const random =Math.floor( Math.random() *  700+1)
-        fetch("https://pokeapi.co/api/v2/pokemon/"+random)
-        .then(res => res.json())
-        .then(data => ignore && setPokemon(data) )
-      } catch (error) {
-        console.log(error);
-      }
-      
-    };
-    fetchResults();
-     return () => {
-      ignore = true;}
-  }, []);
-
   return (
     <div className="card" onClick={handleClick}>
-       <h2>{pokemon.name}</h2>
-      {pokemon.sprites ? <img src={pokemon.sprites.front_default} alt="Random Gif" /> : <p>Loading...</p>}
+       <h2>{pokemon?.name || "Loading..."}</h2>
+      {pokemon?.sprites ? <img src={pokemon.sprites.front_default} alt="Random Gif" /> : <p>Loading...</p>}
           </div>
      
   );
@@ -37,28 +17,44 @@ function Card({updateBoard,index}) {
 
 
 export default function App (){
-  const [board, setBoard]= useState(Array(8).fill(null));
+  const [board, setBoard]= useState([]);
   const [found,setFound] = useState([])
   const [counter, setCounter] = useState(0);
   const [highScore, setHighScore] = useState(0);
+  const [newGame, setNewGame] = useState(false);
+  useEffect(() => {
+    const fetchResults = async () => {
+      try {
+        const promises= Array.from({length:8}, async ()=>{
+        const random =Math.floor( Math.random() *  700+1)
+       const res= await fetch("https://pokeapi.co/api/v2/pokemon/"+random)
+       return res.json();
+        });
+        const results= await Promise.all(promises);
+        setBoard(results)
+      } catch (error) {
+        console.log(error);
+      } 
+    };
+    fetchResults();
+  }, [newGame]);
+
   const restart =() =>{
     setCounter(0)
     setFound([])
-    setBoard(Array(8).fill(null))
+    setBoard([])
+    setNewGame(!newGame)
   }
 
   const updateBoard = (index) =>{
-    const newBoard = [...display];
+    const newBoard = [...board];
         const newFound = found;
-       if (newFound.includes(newBoard[index].key)) {
-        console.log("repeated")
+       if (newFound.includes(newBoard[index].id)) {
         return restart();
        }
-     
-        newFound.push(newBoard[index].key);  
-            setCounter(count => count + 1)
-              
-      newFound.push(newBoard[index].key);
+        newFound.push(newBoard[index].id);  
+            setCounter(count => count + 1)         
+      newFound.push(newBoard[index].id);
       setFound(newFound);
     shuffleArray(newBoard);
         setBoard(newBoard);
@@ -68,17 +64,17 @@ export default function App (){
         setHighScore(counter)
        }
 
-    const display = board.map((_,index) =>{
-        return(
-          <Card key={index} index={index} updateBoard={updateBoard} ></Card>
-        )
-      }) 
+   
     return (
       <>
       <h1>Pokemon Memory Game</h1>
       <h2>Get points by clicking on a pokemon but don't click on any more than once!</h2>
     <div className="Board">
-      {board[0]? board :display}
+     {board.map((pokemon,index) =>{
+        return(
+          <Card key={index} pokemon={pokemon} index={index} updateBoard={updateBoard} ></Card>
+        )
+      })} 
     </div>
      <div className="scores">
       <p className="score">Current Score: {counter} </p>
